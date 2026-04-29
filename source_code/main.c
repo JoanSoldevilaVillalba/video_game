d63a590d24e7f4e8492b4b615364e570c58fa172
JoanSoldevilaVillalba <joansoldevilarosellmendez@gmail.com>
2026-04-29 12:17:24 +0200

    modifications that we have added: - we have modified the .gitignore file. In this file i have explicitly asked git not to push or commit files within code_test_lazyfoo, a direcotyr that contains test code just for me - we have also modified some of the logic of the game: before we were able to calculate the angle between the center of the player and the cursor (based on window). Now the texture that represents the player is "poiting" to wherever the cursor/mouse is.

--- source_code/main.c
+++ source_code/main.c
@@ -142,6 +142,25 @@ void render(SDL_Renderer* renderer, image_representation* image_file) {
 
 }
 
+void render_with_rotation(SDL_Renderer* renderer, image_representation* image_file){
+//keep in mind that in image_file, we have already updated the angle of the image, which is stored inside of image_file already.
+	SDL_FRect destinationRect = {0};
+
+	destinationRect.x = image_file->m_start.m_column;
+
+	destinationRect.y = image_file->m_start.m_row;
+
+	destinationRect.h = image_file->m_dimensions.m_row;
+
+	destinationRect.w = image_file->m_dimensions.m_column;
+
+	//in SDL we need to use SDL_Fpoint
+	SDL_FPoint temporaryPoint = {0};
+	temporaryPoint.x = image_file->m_center.m_column - image_file->m_start.m_column;
+	temporaryPoint.y = image_file->m_center.m_row - image_file->m_start.m_row;
+	SDL_RenderTextureRotated(renderer, image_file->m_texture, NULL, &destinationRect, (double)image_file->m_angle, &temporaryPoint, SDL_FLIP_NONE);
+}
+
 void destroyImageRepresentation(image_representation* image_file) {
 
     if (image_file != NULL) {
@@ -323,7 +342,8 @@ int main(int argc, char* argv[]) {
 
 
                 default_player.m_angle = calculateAngleQuadrent(mouseX, mouseY, default_player.m_center.m_row, default_player.m_center.m_column);
-
+		printf("angle of the player: %f\n", default_player.m_angle);
+		printf("the center of the player: x position: %f  --  y position: %f\n\n\n", default_player.m_center.m_column, default_player.m_center.m_row);
 
             }
 
@@ -331,7 +351,7 @@ int main(int argc, char* argv[]) {
 SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_RenderClear(renderer);
 
-        render(renderer, &default_player);
+        render_with_rotation(renderer, &default_player);
 
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_RenderPoint(renderer, default_player.m_center.m_column, default_player.m_center.m_row);
