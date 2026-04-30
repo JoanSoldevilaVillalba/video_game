f90996b328de4b1fdb0dcb91a44d72f11433e746
JoanSoldevilaVillalba <joansoldevilarosellmendez@gmail.com>
2026-04-30 09:10:04 +0200

    added char paths for the punch left and punch right animations. added two arrays of type image_representation that contain the images for both animations from before.

--- source_code/main.c
+++ source_code/main.c
@@ -322,12 +322,25 @@ int main(int argc, char* argv[]) {
 
     SDL_Renderer* renderer = NULL;
 
+	/*the following image_representation is just for the defaut, without any animations*/
     image_representation default_player = { 0 };
+	/*the following we have two arrays of image_representations for two different types of animations*/
+	image_representation player_punch_left[8];
+	image_representation player_punch_right[8];
 
     int exit_code = 0;
-
+/*This is the default image, just the normal image*/
     const char* image_path = "../videoGameImatges/default.png";
+/*the following arrays of char strings are for animtaions, speicifly for punching left and punching right*/
+    const char* image_path_punch_left[] = {"../videoGameImatges/punch_left/first.png","../videoGameImatges/punch_left/second.png",
+    "../videoGameImatges/punch_left/third.png","../videoGameImatges/punch_left/fourth.png","../videoGameImatges/punch_left/fith.png",
+    "../videoGameImatges/punch_left/sixth.png","../videoGameImatges/punch_left/seventh.png","../videoGameImatges/punch_left/vuit.png"
+    };
 
+    const char* image_path_punch_right[] = {"../videoGameImatges/punch_right/first.png","../videoGameImatges/punch_right/second.png",
+    "../videoGameImatges/punch_right/third.png","../videoGameImatges/punch_right/fourth.png","../videoGameImatges/punch_right/fith.png",
+    "../videoGameImatges/punch_right/sixth.png","../videoGameImatges/punch_right/seventh.png","../videoGameImatges/punch_right/vuit.png"
+    };
     exit_code = init(&window, &renderer);
 
     if (exit_code != 0) {
@@ -339,6 +352,17 @@ int main(int argc, char* argv[]) {
     }
 
     exit_code = loadMedia(&default_player, image_path, renderer, 0.0f, 0.0f);
+/*first punch left animation*/
+	for (int i =0;i<8;i++){
+
+	loadMedia(&player_punch_left[i], image_path_punch_left[i], renderer, 0.0f, 0.0f);
+	}
+	for (int i =0;i<8;i++){
+
+	loadMedia(&player_punch_right[i], image_path_punch_right[i], renderer, 0.0f, 0.0f);
+
+	}
+
 
     if (exit_code != 0) {
 
