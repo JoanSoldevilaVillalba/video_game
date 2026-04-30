b0844d328e8185085f1f2d501ab7e32167262f8b
JoanSoldevilaVillalba <joansoldevilarosellmendez@gmail.com>
2026-04-30 10:39:08 +0200

    We are creating a new branch that is specific for optmitzing the code to increase performance. In this case we have eliminated m_center from image_representation, we simply just have to use m_start and some simple arithmetic math, which is better thann having to access memory. We also added some code in eventhandler function to make sure that we only calculate the angle when it is absolutly necessary (when mouse or player moves). This is because in order to calcualte angle we need to call atan and perform other costly operations.

--- source_code/main.c
+++ source_code/main.c
@@ -19,11 +19,11 @@ typedef struct {
 
 typedef struct {
 
-
-    image_point m_center;
-
     image_point m_start;
 
+	//we could eliminate m_dimensions from image_representation struct because it is already in m_texture, but for now we are just going to maintain it, just because i think that just having two floats
+	//... is less costly than having to do a memory access from m_texture. In the current way though we are going to put more pressure in the registers of our processor, and it is also possible that there
+	//is a lot of memory being used in one point of the program, having to mmove these values into memory rather than a register, for now we will just leave it like this.
     image_point m_dimensions;
 
     float m_angle;
@@ -33,19 +33,21 @@ typedef struct {
 
 }image_representation;
 
+/*
+void calculateCenter(image_representation* image_file, SDL_FPoint* center_point) {
 
-void  calculateCenter(image_representation* image_file) {
 
     if (image_file != NULL) {
 
-        image_file->m_center.m_row = image_file->m_dimensions.m_row / 2 + image_file->m_start.m_row;
+        center_poin->y = image_file->m_dimensions.m_row / 2 + image_file->m_start.m_row;
 
-        image_file->m_center.m_column = image_file->m_dimensions.m_column / 2 + image_file->m_start.m_column;
+        center_point->x = image_file->m_dimensions.m_column / 2 + image_file->m_start.m_column;
 
     }
 
+return temporary;
 
-}
+}*/
 int loadFromFile(const char* image_path, image_representation* image_file, SDL_Renderer* renderer, float startingX, float startingY) {
 
     int exit_code = 0;
@@ -78,8 +80,6 @@ int loadFromFile(const char* image_path, image_representation* image_file, SDL_R
             image_file->m_dimensions.m_row = temporarySurface->h;
 
             image_file->m_dimensions.m_column = temporarySurface->w;
-
-            calculateCenter(image_file);
 
         }
 
@@ -156,8 +156,14 @@ void render_with_rotation(SDL_Renderer* renderer, image_representation* image_fi
 
 	//in SDL we need to use SDL_Fpoint
 	SDL_FPoint temporaryPoint = {0};
-	temporaryPoint.x = image_file->m_center.m_column - image_file->m_start.m_column;
-	temporaryPoint.y = image_file->m_center.m_row - image_file->m_start.m_row;
+
+	float center_m_row = image_file->m_dimensions.m_row / 2 + image_file->m_start.m_row;
+
+        float center_m_column = image_file->m_dimensions.m_column / 2 + image_file->m_start.m_column;
+
+
+	temporaryPoint.x = center_m_column - image_file->m_start.m_column;
+	temporaryPoint.y = center_m_row - image_file->m_start.m_row;
 	SDL_RenderTextureRotated(renderer, image_file->m_texture, NULL, &destinationRect, (double)image_file->m_angle, &temporaryPoint, SDL_FLIP_NONE);
 }
 
@@ -172,11 +178,6 @@ void destroyImageRepresentation(image_representation* image_file) {
             image_file->m_texture = NULL;
 
         }
-
-
-        image_file->m_center.m_row = 0.0f;
-
-        image_file->m_center.m_column = 0.0f;
 
         image_file->m_start.m_row = 0.0f;
 
@@ -231,7 +232,7 @@ float calculateAngleQuadrent(float mouseX, float mouseY, float row_user, float c
 
     if(angle_final<0){
 
-		angle_final+=360.0f;
+	angle_final+=360.0f;
 
     }
 
@@ -242,57 +243,68 @@ float calculateAngleQuadrent(float mouseX, float mouseY, float row_user, float c
 
 bool eventHandler(SDL_Event* event,float* mouseX, float* mouseY, image_representation* default_player){
 	bool quit = false;
+	bool movement = false;
+        if (event->type == SDL_EVENT_QUIT) {
 
-            if (event->type == SDL_EVENT_QUIT) {
 
-                quit = true;
-
+	return true;
             }
-            else {
+        if (event->type == SDL_EVENT_MOUSE_MOTION) {
+			*(mouseX) = event->motion.x;
+			*(mouseY) = event->motion.y;
+			movement = true;
+                }else if (event->type == SDL_EVENT_KEY_DOWN) {
 
-                if (event->type == SDL_EVENT_MOUSE_MOTION) {
+			switch(event->key.key){
 
-                    uint32_t buttons = SDL_GetMouseState(mouseX, mouseY);
+				case SDLK_UP:
 
-                }
+					default_player->m_start.m_row -=2;
 
-                if (event->type == SDL_EVENT_KEY_DOWN) {
-                    //every time that we update the images staring rendering positino, we are also going to have to update its center
+					movement = true;
 
-                    if (event->key.key == SDLK_UP) {
+					break;
 
-                        default_player->m_start.m_row -= 2;
+				case SDLK_DOWN:
 
-                        default_player->m_center.m_row -= 2;
+					default_player->m_start.m_row +=2;
+
+					movement = true;
+
+					break;
+				case SDLK_LEFT:
+
+					default_player->m_start.m_column -=2;
+
+					movement = true;
+
+					break;
+
+				case SDLK_RIGHT:
+					default_player->m_start.m_column +=2;
+
+					movement = true;
+
+					break;
+				default:
+
+					SDL_Log("Error, you have pressed a button that is not yet allowed");
+
+					break;
+			}
 
                     }
-                    if (event->key.key == SDLK_DOWN) {
 
-                        default_player->m_start.m_row += 2;
+	if(movement){
 
-                        default_player->m_center.m_row += 2;
+		float center_m_row = (default_player->m_dimensions.m_row/2.0f) + default_player->m_start.m_row;
 
-                    }
-                    if (event->key.key == SDLK_LEFT) {
+		float center_m_column = (default_player->m_dimensions.m_column/2.0f) + default_player->m_start.m_column;
 
-                        default_player->m_start.m_column -= 2;
+	        default_player->m_angle = calculateAngleQuadrent(*(mouseX), *(mouseY), center_m_row,center_m_column);
 
-                        default_player->m_center.m_column -= 2;
+	}
 
-                    }
-                    if (event->key.key == SDLK_RIGHT) {
-
-                        default_player->m_start.m_column += 2;
-
-                        default_player->m_center.m_column += 2;
-
-                    }
-
-
-                }
-
-}
-         default_player->m_angle = calculateAngleQuadrent(*(mouseX), *(mouseY), default_player->m_center.m_row, default_player->m_center.m_column);
 
 	return quit;
 
@@ -309,7 +321,11 @@ void rendering(SDL_Renderer* renderer, image_representation* default_player){
 
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 
-	SDL_RenderPoint(renderer, default_player->m_center.m_column, default_player->m_center.m_row);
+	float center_m_row = default_player->m_dimensions.m_row/2 + default_player->m_start.m_row;
+
+	float center_m_column = default_player->m_dimensions.m_column/2 + default_player->m_start.m_column;
+
+	SDL_RenderPoint(renderer, center_m_column, center_m_row);
 
         SDL_RenderPresent(renderer);
 
@@ -350,7 +366,8 @@ int main(int argc, char* argv[]) {
         return exit_code;
 
     }
-
+//////////above this line in the main, we dont need to do anything to optmitze our code, or atleast it won't affect the performance as mush if we focus on what is down below
+///i understand that i should focus on the main loop, but whatever
     exit_code = loadMedia(&default_player, image_path, renderer, 0.0f, 0.0f);
 /*first punch left animation*/
 	for (int i =0;i<8;i++){
@@ -390,16 +407,14 @@ int main(int argc, char* argv[]) {
 
     while (!quit) {
 
-        while (SDL_PollEvent(&event)) {
+        while (SDL_PollEvent(&event)){
 
 		quit = eventHandler(&event, &mouseX, &mouseY, &default_player);
-
-            }
-
+        }
 
 		rendering(renderer, &default_player);
 
-        }
+    }
 
 
         destroyImageRepresentation(&default_player);
