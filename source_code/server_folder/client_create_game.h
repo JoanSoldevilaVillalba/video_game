8993f295482b4f666d84adb49c8ea45a09611e07
JoanSoldevilaVillalba <joansoldevilarosellmendez@gmail.com>
2026-05-06 23:08:02 +0200

    added networking .h file for client side

---
+++ source_code/server_folder/client_create_game.h
@@ -0,0 +1,10 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+void setupConnection(int* client_file_descriptor,struct sockaddr_in* server_address, int port);
+int send_data_to_server(char* buffer_r,char* buffer_s , int client_file_descriptor);
+void handleServerCommunication(int server_port);
