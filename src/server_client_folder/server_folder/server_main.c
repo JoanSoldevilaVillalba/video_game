e25d5b1cfdd56496375a928e95b8ecbe9888edd0
JoanSoldevilaVillalba <joansoldevilarosellmendez@gmail.com>
2026-08-15 13:52:52 +0200

    fix/feat: eliminated auxilary logic functions from server_main

--- src/server_client_folder/server_folder/server_main.c
+++ src/server_client_folder/server_folder/server_main.c
@@ -26,7 +26,7 @@ RANDOM_MESSAGE = 2,
 MENU_PREPERATION = 3
 
 }FIRST_LAYER;
-//we are going to make ssure that the server only has one layer , because the server is a memoryless server
+
 
 typedef struct{
 
@@ -50,104 +50,7 @@ pthread_mutex_t mutex_thread_counter;
 
 pthread_cond_t conditional_variable;
 
-int counter_thread; //automaticlyh it is goingto be initilized to zero
-
-void reverse(char* pointer){
-
-	int length = strlen(pointer);
-
-	for(int i = 0;i<length;i++){
-
-		char temp = *(pointer + i);
-
-		*(pointer + i) = *(pointer + length - 1 - i);
-
-		*(pointer + length - 1 - i) = temp;
-
-	}
-
-}
-
-int number_to_char(char* pointer, int temporary){
-
-	int i = 0;
-
-	int residual = 0;
-
-
-	while(temporary>=10){
-
-		residual = temporary % 10;
-		//residual is the first number that we are receving
-
-		temporary = temporary/10;
-
-
-		*(pointer + i) = residual +'0';
-
-		i++;
-
-	}
-
-	*(pointer + i) = temporary + '0';
-
-	++i;
-
-	*(pointer + i)  = '\0';
-
-	reverse(pointer);
-
-	i = i - 1;
-
-	return i;
-
-}
-
-
-char* create_game(int temporary_fd, char* buffer_receive, int* result_function, int* index_game, game_struct_players* game_list){
-
-	int i = 0;
-
-	for(i;i<MAX_GAMES_SIZE;i++){
-
-		pthread_mutex_lock(&mutex_game_list);
-
-		if((game_list+i)->game_id!=-1 && (game_list+i)->second_player==-1){
-
-			(game_list+i)->second_player = temporary_fd;
-			*(result_function) = 1;
-			*(index_game) = i;
-			pthread_cond_signal(&(game_list + i)->game_condition);
-			pthread_mutex_unlock(&mutex_game_list);
-			return "0|1|you have succesfuly entered a game";
-
-		}else if((game_list+i)->game_id==-1){
-
-			(game_list+i)->game_id = i;
-			(game_list+i)->first_player = temporary_fd;
-			*(result_function) = 2;
-			*(index_game) = i;
-			pthread_mutex_unlock(&mutex_game_list);
-			return "0|2|you have succesfuly created a game";
-
-		}
-
-
-	pthread_mutex_unlock(&mutex_game_list);
-
-
-
-	}
-
-
-
-	*(result_function) = 3 ;
-	*(index_game) = -1;
-
-	return "0|3|All games are occupied, try again later";
-
-}
-
+int counter_thread;
 
 typedef struct{
 
@@ -157,79 +60,7 @@ game_struct_players* pointer_list_game;
 
 }struct_client;
 
-bool validate_message_length(const char* temporary_pointer){
 
-        if(BUFFER_SIZE <= strlen(temporary_pointer)){
-
-                return false;
-
-        }else{
-
-                return true;
-
-        }
-
-}
-
-
-
-ssize_t send_validated_message(const char* temporary_pointer, char buffer[BUFFER_SIZE], int client_file_descriptor){
-
-        bool correct_length = validate_message_length(temporary_pointer);
-
-        ssize_t result = 0;
-
-        if(correct_length == true){
-
-                strncpy(buffer, temporary_pointer, strlen(temporary_pointer));
-
-                buffer[strlen(temporary_pointer)] = '\0';
-
-                result = send_framed_message(client_file_descriptor, buffer, (uint32_t)strlen(temporary_pointer));
-
-                if(result == -1){
-
-                        printf("Unable to send message to server: %s\n", strerror(errno));
-
-                        result = -1;
-
-                }
-
-        }else{
-
-                printf("Error, the following message exceeds the BUFFER_SIZE limit: %s\n", temporary_pointer);
-
-                result = -2;
-
-        }
-
-
-
-        return result;
-
-}
-
-ssize_t receive_validated_message(char buffer[BUFFER_SIZE], int client_file_descriptor){
-
-	//we first need to receive the information
-
-	ssize_t result_bytes_receive = receive_framed_message(client_file_descriptor, buffer, (ssize_t) BUFFER_SIZE);
-
-	const char* temporary_pointer = buffer;
-
-	bool validated = validate_message_length = (temporary_pointer);
-
-	if(result_bytes_receive == -1 || !validated){
-
-	//possible broken connection, all bytes where not received (initial length is not equal to message received) ...
-
-		return -1;
-
-	}
-
-	return result_bytes_receive;
-
-}
 
 void* handle_client(void* arg){
 
@@ -288,8 +119,7 @@ void* handle_client(void* arg){
 
 				temporary_pointer = create_game(client->socket_fd, buffer_receive, &result, &index_game, client->pointer_list_game);
 
-					bytes_result = send_validated_message(temporary_pointer, buffer_send, client->socket_fd);
-
+				bytes_result = send_validated_message(temporary_pointer, buffer_send, client->socket_fd);
 
 				if(bytes_result == -1){
 
