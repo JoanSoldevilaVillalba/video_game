58cc935e9bd9c3760e017522edb3f4b9a94bac51
JoanSoldevilaVillalba <joansoldevilarosellmendez@gmail.com>
2026-08-15 18:17:47 +0200

    fix: deleted global variable, struct declarations and library includes from server_main.c (they are in server.h)

--- src/server_client_folder/server_folder/server_main.c
+++ src/server_client_folder/server_folder/server_main.c
@@ -1,68 +1,4 @@
-#include <netinet/in.h>
-#include <errno.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <pthread.h>
-
-#include "server_send_data.h"
-
-#include "server_logic.h"
-
-#define BUFFER_SIZE 64
-
-#define MAX_GAMES_SIZE 2
-
-#define MAX_CLIENT_THREADS (MAX_GAMES_SIZE*2)
-
-
-typedef enum{
-
-ENTERING_CREATING_GAME = 0,
-
-QUIT = 1,
-
-RANDOM_MESSAGE = 2,
-
-MENU_PREPERATION = 3
-
-}FIRST_LAYER;
-
-
-typedef struct{
-
-int first_player;
-
-int second_player;
-
-char name[62];
-
-int game_id;
-
-pthread_cond_t game_condition;
-
-}game_struct_players;
-
-
-
-pthread_mutex_t mutex_game_list;
-
-pthread_mutex_t mutex_thread_counter;
-
-pthread_cond_t conditional_variable;
-
-int counter_thread;
-
-typedef struct{
-
-int socket_fd;
-
-game_struct_players* pointer_list_game;
-
-}struct_client;
-
-
+#include "server.h"
 
 void* handle_client(void* arg){
 
@@ -205,7 +141,7 @@ void* handle_client(void* arg){
 
 				strncpy(buffer_send, temporary_buffer, BUFFER_SIZE);
 
-				temporary_poitner = buffer_send;
+				temporary_pointer = buffer_send;
 
 				break;
 
