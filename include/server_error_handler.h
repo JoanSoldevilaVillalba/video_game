9a7a0f8dcd87eab416641a51a256e91cf691a0c5
JoanSoldevilaVillalba <joansoldevilarosellmendez@gmail.com>
2026-08-19 23:50:09 +0200

    feat: adding main error handler for server. This handler will prevent bad client-server behaviour aswell as inner server undefined/bad behavouir

---
+++ include/server_error_handler.h
@@ -0,0 +1,4 @@
+#include <string.h>
+#include <errno.h>
+
+void* main_error_handler(void* arg, int error_value);
---
+++ src/server_client_folder/server_folder/server_error_handler.c
@@ -0,0 +1,9 @@
+#include "server_error_handler.h"
+
+void* main_error_handler(void* arg, int error_value){
+
+
+
+
+
+}
