ba2097e7c31c3e21b6f762e549718b7e5af04bfb
JoanSoldevilaVillalba <joansoldevilarosellmendez@gmail.com>
2026-08-20 16:14:48 +0200

    feat: started basic structure for main error handeling function and its declaration

--- include/server_error_handler.h
+++ include/server_error_handler.h
@@ -1,4 +1,50 @@
 #include <string.h>
 #include <errno.h>
 
-void* main_error_handler(void* arg, int error_value);
+typedef enum{
+
+	COMM_ERROR = 0, //error based on communication protocol
+
+	INN_ERROR = 1 //error inner server, client does not have to do anything with these types of errors
+
+}ERROR_LAYER_1;
+
+
+typedef enum{
+
+	SYSCALL_ERROR = 0,//possible syscall error 
+
+	ABS_LEN_ERROR = 1 //length error on incoming message
+
+
+}ERROR_LAYER_COMM;
+
+
+typedef enum{
+
+	ABS_OVERFLOW_BUFF = 0 //buffer overflow, server wants to send a message that is too big
+
+	ABS_LEN_ERROR = 1, //lenght of message
+
+	UD_POINTER = 2, //undefined pointer
+
+
+}ERROR_LAYER_INN;
+
+typedef struct{
+
+	//to represent an error, we are going to need a buffer, that holds the message that is going to be printed to the standard ouput
+	//aswell as two numbers that represent what type of error has occured
+
+	char buffer_error[BUFFER_SIZE];
+
+	int type1_error;
+
+	int type2_error;
+
+}error_node;
+
+//for now we are going to leave it as a void pointer, we might change the error_node  data structure
+
+
+void* main_error_handler(void* arg);
--- src/server_client_folder/server_folder/server_error_handler.c
+++ src/server_client_folder/server_folder/server_error_handler.c
@@ -1,8 +1,78 @@
 #include "server_error_handler.h"
 
-void* main_error_handler(void* arg, int error_value){
+void* main_error_handler(void* arg){
+
+	error_node* error = (error_node*)arg;
+
+	//type1 helps us understand wheter we are in comm or inn:
+
+	//we are going to use a swtich case, for scalibility
+
+	switch(error->type1_error){
+
+		case COMM_ERROR:
+
+			//these types of errors happen when the communication has been broekn, syscalls and things taht the client has sent over taht are incorrect
+
+			comm_handler(error);
+
+			break;
+
+		case INN_ERROR:
+
+			//these are not communication based errors
+
+			inn_handler(error);
+
+			break;
 
 
+		default:
+
+
+			break;
+
+
+
+	}
+
+
+
+}
+
+
+
+void* comm_handler(error_node* error){
+
+	printf("---- We are in communication error handler ----\n");
+	printf("Incoming error was due to the following: \n");
+	switch(type2_error){
+
+		case SYSCALL_ERROR:
+
+			printf("Syscall failure, errno is giving us the following: %s , errno value: %d\n", strerror(errno), errno);
+
+			//for this type of error, we are just going to have to close the connectoin and ask the user to communicate with the server another time
+
+			break;
+
+
+		case ABS_LEN_ERROR:
+
+			printf("Incoming message length error\n");
+
+			//in this case, i don't really know what i should do, should i create a new case in the while loop just for error handeling for comm?
+
+			break;
+
+	}
+
+
+
+}
+
+
+void* inn_handler(error_node* error){
 
 
 
