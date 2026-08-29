d6c8d2350f2bac175e79cf5f5a168824f38e578f
JoanSoldevilaVillalba <joansoldevilarosellmendez@gmail.com>
2026-08-29 22:52:24 +0200

    fix: added function declaration from previous commit statment (abstraction function added for menu_preperation)

--- include/server_logic.h
+++ include/server_logic.h
@@ -37,3 +37,5 @@ char* waiting_for_player(struct_client* client, int* index_game,int* index_playe
 void handlePE(size_t* result, char* buffer_receive,char* buffer_error, bool* quit, int* first_number);
 
 char* create_game(int temporary_fd, int* result_function, int* index_game, game_struct_players* game_list);
+
+int menu_preperation_validation(struct_client* client, int index, char* temporary_buffer, char* buffer_error);
