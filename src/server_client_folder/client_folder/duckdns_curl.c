d4a7d73121fe91fe38d9aeec1e20896e86cbbd5a
JoanSoldevilaVillalba <joansoldevilarosellmendez@gmail.com>
2026-09-08 07:51:02 +0200

    feat: added write_callback function to save and parse incoming transfer from defined url

--- src/server_client_folder/client_folder/duckdns_curl.c
+++ src/server_client_folder/client_folder/duckdns_curl.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "curl/curl.h"
 
 #define BUFFER_SIZE 64
@@ -9,6 +11,48 @@
     #define GLOBAL_INIT_BIT CURL_GLOBAL_ALL
 #endif
 
+
+struct MemoryStruct{
+
+char* memory;
+
+size_t size;
+
+char buffer_error[BUFFER_SIZE];
+
+};
+
+static size_t write_callback(char *content_received, size_t size, size_t nmemb, void *user_type_data){
+
+	size_t realsize = size * nmemb;
+
+	struct MemoryStruct* mem = ( struct MemoryStruct*)user_type_data;
+
+	char* temporary_ptr = realloc(mem->memory, mem->size + realsize +1);
+
+	if(temporary_ptr == NULL){
+
+		snprintf(mem->buffer_error, BUFFER_SIZE, "%s", "Error, not enough memory for received transfer");
+
+		return 0;
+
+	}
+
+	mem->memory = temporary_ptr;
+
+	memcpy(&mem->memory[mem->size], content_received, realsize);
+
+	mem->size += realsize;
+
+	mem->memory[mem->size] = 0;
+
+	printf("We have receveid the following amount of bytes: %d\n",(int)(size * nmemb));
+
+	return size * nmemb; //amount of bytes that this call of write_callback has received
+
+}
+
+
 int main(void){
 
 	char buffer_domain[BUFFER_SIZE];
@@ -17,15 +61,51 @@ int main(void){
 
 	char buffer_token[BUFFER_SIZE];
 
+	struct MemoryStruct user_data;
+
+	user_data.memory = malloc(1);
+
+	user_data.size = 0;
+
+	memset(user_data.buffer_error, '\0',BUFFER_SIZE);
+
 	CURL* handler = NULL;
+
+	CURLcode res;
 
 	curl_global_init(GLOBAL_INIT_BIT);
 
 		handler = curl_easy_init();
 
+			if(handler){
+
+				curl_easy_setopt(handler, CURLOPT_URL, "https://example.com/");
+
+				curl_easy_setopt(handler, CURLOPT_WRITEDATA, (void*)&user_data);
+
+				curl_easy_setopt(handler, CURLOPT_WRITEFUNCTION, write_callback);
+
+				res = curl_easy_perform(handler);
+
+				if(res!=CURLE_OK){
+
+					printf("Error calling curl_easy_perform: %s\n", curl_easy_strerror(res));
+
+				}else{
 
 
-		curl_easy_cleanup(handler);
+					printf("The transfer was a success\n");
+
+					printf("This is the content: \n %s", user_data.memory);
+
+
+				}
+
+			curl_easy_cleanup(handler);
+
+			}else{
+				printf("Error, curl_easy_init faild, returned null pointer\n");
+			}
 
 	curl_global_cleanup();
 
