a35d46094ac88cd99cd446c724e44b75a3de8f22
JoanSoldevilaVillalba <joansoldevilarosellmendez@gmail.com>
2026-09-08 09:14:53 +0200

    feat: added file to .gitignore that contains private token for duckdns dynamic resolver (ip address server resolution)

--- src/server_client_folder/client_folder/duckdns_curl.c
+++ src/server_client_folder/client_folder/duckdns_curl.c
@@ -18,8 +18,23 @@ char* memory;
 
 size_t size;
 
-char buffer_error[BUFFER_SIZE];
+};
 
+typedef  enum{
+
+DUCK_DNS_URL = 0,
+
+DUCK_DNS_DOMAIN = 1,
+
+DUCK_DNS_TOKEN = 2
+
+
+}webID;
+
+const char* string_web_holder[]={
+[DUCK_DNS_URL]"https://www.duckdns.org/update?domains=%s&token=%s&verbose=true",
+[DUCK_DNS_DOMAIN] = "video-game-project",
+[DUCK_DNS_TOKEN]="2ff8342c-836c-4712-aa25-ff5c90cd43a5"
 };
 
 static size_t write_callback(char *content_received, size_t size, size_t nmemb, void *user_type_data){
@@ -32,7 +47,7 @@ static size_t write_callback(char *content_received, size_t size, size_t nmemb, 
 
 	if(temporary_ptr == NULL){
 
-		snprintf(mem->buffer_error, BUFFER_SIZE, "%s", "Error, not enough memory for received transfer");
+//		snprintf(mem->buffer_error, BUFFER_SIZE, "%s", "Error, not enough memory for received transfer");
 
 		return 0;
 
@@ -55,11 +70,22 @@ static size_t write_callback(char *content_received, size_t size, size_t nmemb, 
 
 int main(void){
 
+	char buffer_url[BUFFER_SIZE+BUFFER_SIZE];
+
 	char buffer_domain[BUFFER_SIZE];
 
-	char buffer_error[BUFFER_SIZE];
+	char buffer_token[BUFFER_SIZE+BUFFER_SIZE];
 
-	char buffer_token[BUFFER_SIZE];
+	char buffer_error[CURL_ERROR_SIZE];
+
+	snprintf(buffer_domain, BUFFER_SIZE, "%s", string_web_holder[DUCK_DNS_DOMAIN]);
+
+	snprintf(buffer_token, BUFFER_SIZE+BUFFER_SIZE, "%s", string_web_holder[DUCK_DNS_TOKEN]);
+
+	snprintf(buffer_url, BUFFER_SIZE+BUFFER_SIZE,string_web_holder[DUCK_DNS_URL], buffer_domain, buffer_token);
+
+	printf("Here we have the url that we are going to retreive data from : %s\n", buffer_url);
+
 
 	struct MemoryStruct user_data;
 
@@ -67,11 +93,12 @@ int main(void){
 
 	user_data.size = 0;
 
-	memset(user_data.buffer_error, '\0',BUFFER_SIZE);
-
 	CURL* handler = NULL;
 
 	CURLcode res;
+
+	printf("size of curl error size: %d", (int)CURL_ERROR_SIZE);
+
 
 	curl_global_init(GLOBAL_INIT_BIT);
 
@@ -79,24 +106,44 @@ int main(void){
 
 			if(handler){
 
-				curl_easy_setopt(handler, CURLOPT_URL, "https://example.com/");
+				curl_easy_setopt(handler, CURLOPT_URL,buffer_url);
+
+				curl_easy_setopt(handler,CURLOPT_VERBOSE, 1L);
 
 				curl_easy_setopt(handler, CURLOPT_WRITEDATA, (void*)&user_data);
 
 				curl_easy_setopt(handler, CURLOPT_WRITEFUNCTION, write_callback);
 
+				curl_easy_setopt(handler, CURLOPT_ERRORBUFFER, buffer_error);
+
 				res = curl_easy_perform(handler);
 
 				if(res!=CURLE_OK){
 
-					printf("Error calling curl_easy_perform: %s\n", curl_easy_strerror(res));
+					printf("Error calling curl_easy_perform: %s, extra: %s\n", curl_easy_strerror(res), buffer_error);
 
 				}else{
 
 
 					printf("The transfer was a success\n");
 
-					printf("This is the content: \n %s", user_data.memory);
+					printf("This is the content: \n %s\n\n", user_data.memory);
+
+					char* domain = NULL;
+
+					const char* delimiter = "domain";
+
+					domain = strstr(user_data.memory, delimiter);
+
+					if(domain){
+
+						printf("We have found the domain. Here is the address: %p", domain);
+
+					}else{
+
+						printf("We were not able to find the following substring: %s", delimiter);
+
+					}
 
 
 				}
@@ -106,6 +153,8 @@ int main(void){
 			}else{
 				printf("Error, curl_easy_init faild, returned null pointer\n");
 			}
+
+	free(user_data.memory);
 
 	curl_global_cleanup();
 
