faa0972d0cde7efd1c4537e7fef9e37d017465f4
JoanSoldevilaVillalba <joansoldevilarosellmendez@gmail.com>
2026-09-25 01:34:31 +0200

    fix/feat: continued packet receiving robust implementation aswell as defined specific string holders for possible poll and recv errors. Added comments for specific error types

--- src/server_client_folder/server_folder/server_send_data.c
+++ src/server_client_folder/server_folder/server_send_data.c
@@ -1,55 +1,110 @@
 #include "server_send_data.h"
+#include "server_send_data.h"
 
-int handle_poll_error(pfd* pstructure_pointer, int return_value_poll, short correct_event){
+const char* error_string_network_poll[] = {
+[SYS_POLL] = "poll(): an error event was detected",
+[POLL_RDHUP] = "poll(): peer performed a half-close (POLLRDHUP)",
+[POLL_HUP] = "poll(): connection hangup detected (POLLHUP)",
+[POLL_ERR] = "poll(): error condition detected (POLLERR)",
+[POLL_NVAL] = "poll(): invalid file descriptor (POLLNVAL)",
+[POLL_IN] = "poll(): data is available to read (POLLIN)",
+[ERRNO_VALUES_POLL] = "poll(): system call failed; check errno",
+[TIME_EXP_POLL] ="poll(): timeout expired; no events occurred"
+};
+
+const char* error_string_network_recv[]={
+[INVALID_FD_RECV] = "Invalid file descriptor is being used for current thread connection",
+[INVALID_ARG_RECV] = "Invalid argument was passed to recv sys call",
+[NO_SETUP_RECV] = "Connection was not established with the client"
+};
+
+const char* error_string_network_send[]={
+
+};
+
+int handle_poll_error(pfd* pstructure_pointer, int return_value_poll){
 
 	int result = -1;
 
 	if(return_value_poll>0){
 
-		switch(pstructure_pointer->revents){
+
+		switch (pstructure_pointer->revents) {
 
 			case POLLRDHUP:
 
-				snprintf(buffer_error, BUFFER_SIZE; "%s", error_string_holder[]);
+				snprintf(buffer_error, BUFFER_SIZE, "%s",error_string_network_poll[POLL_RDHUP]);
 
-				break;
+				result = SOFT_SHUTDOWN;
+
+			break;
 
 			case POLLHUP:
 
-				snprintf(buffer_error, BUFFER_SIZE, "%s", error_string_holder[SYS_POLL]);
+				snprintf(buffer_error, BUFFER_SIZE, "%s",error_string_network_poll[POLL_HUP]);
+
+				/*en realitzar la crida sincrona, el sistema operatiu, el seu packet network 
+manager, veu que estem esperant per un packet duna connexio que ja s'havia tancat. De manera 
+que sense haver de rebre cap paquet, el network manager respon al nostre programma indicant que 
+la connexio socket ja esta desconnectada*/
+
+				result = HARD_SHUTDOWN;
 
 				break;
 
 			case POLLERR:
 
-				snprintf(buffer_error, BUFFER_SIZE, "%s", error_string_holder[SYS_POLL]);
+				//this error can occur due to asyncronus reasons:
+
+				/*
+				when calling poll function, our program is invoking the operating system to perform a system call, puttuing our thread to 
+				sleep untill an event occurs. The operating system then hands these to the network packet manager, which receives an error for example a RST packet,
+				*/
+
+				snprintf(buffer_error, BUFFER_SIZE, "%s",error_string_network_poll[POLL_ERR]);
+
+				result = HARD_SHUTDOWN;
 
 				break;
 
 			case POLLNVAL:
 
-				snprintf(buffer_error, BUFFER_SIZE, "%s", error_string_holder[SYS_POLL]);
+				//invalid request, meaning the file descriptor is not pointing to an actual file
+
+				snprintf(buffer_error, BUFFER_SIZE, "%s",error_string_network_poll[POLL_NVAL]);
+
+				result = HARD_SHUTDOWN;
 
 				break;
 
-			case correct_event:
+			case POLLIN:
 
-				snprintf(buffer_error, BUFFER_SIZE, "%s", error_string_holder[SYS_POLL]);
+				//this is the correct case, no error, therfore NO_SHUTDOWN
 
-				result = 0;
+				snprintf(buffer_error, BUFFER_SIZE, "%s",error_string_network_poll[POLL_IN]);
+
+				result = NO_SHUTDOWN;
 
 				break;
 
-		}
-
+			}
 
 	}else if(return_value_poll == -1){
 
-		snprintf(buffer_error, BUFFER_SIZE, "%s", error_string_holder[ERRNO_VALUES]);
+		//errno error, this is not a connection problem, or an abstract socket problem, but rather the actual poll procedure failed due to memory space or something else
+
+		snprintf(buffer_error, BUFFER_SIZE, "%s", error_string_network_poll[ERRNO_VALUES]);
+
+		result = HARD_SHUTDOWN;
+
 
 	}else if(return_value_poll == 0){
 
-		snprintf(buffer_error, BUFFER_SIZE, "%s", error_string_holder[TIME_EXP_POLL]);
+		//time expired, client failed to send a message to use (constant packae drop for example)
+
+		snprintf(buffer_error, BUFFER_SIZE, "%s", error_string_netowrk_poll[TIME_EXP_POLL]);
+
+		result = SOFT_SHUTDOWN;
 
 	}
 
@@ -68,11 +123,17 @@ int error_handler_recv(int result_receive){
 
 			result = 0;
 
+			result = NO_SHUTDOWN;
+
 			break;
 
 		case EBDAF:
 
-			snprintf(buffer_error, BUFFER_SIZE, "%s",error_string_holder[]);
+			snprintf(buffer_error, BUFFER_SIZE, "%s",error_string_network_recv[]);
+
+			//using an invalid file descriptor: negative integer, not initialized or opened befor
+
+			result = HARD_SHUTDOWN;
 
 			break;
 
@@ -80,27 +141,28 @@ int error_handler_recv(int result_receive){
 
 			snprintf(buffer_error, BUFFER_SIZE, "%s",error_string_holder[]);
 
-			break;
+			//we have passed an invalid argument to recv
 
-
-		case ECONNREFUSED:
-
-			snprintf(buffer_error, BUFFER_SIZE, "%s", error_string_holder[]);
+			result = HARD_SHUTDOWN;
 
 			break;
 
 		case ENOTCONN:
 
+			//no connection has been done to the file descriptor/socket
+			//this one will probably never happen b
+
 			snprintf(buffer_error, BUFFER_SIZE, "%s", error_string_holder[]);
 
-			//we are in tcp, and we have not already established the connection
+			result = HARD_SHUTDOWN;
 
 			break;
 
+			//we are in tcp, and we have not already established the connection
+
 		default:
 
-			//nothing really happned, we have received a number of bytes
-			result = result_receive;
+			result = NO_SHUTDOWN;
 
 			break;
 
@@ -120,7 +182,7 @@ ssize_t read_all(int temporary_fd, char* buffer, char* buffer_error  , ssize_t l
 
 	pfd.fd = temporary_fd;
 
-	pfd.events = (POLLIN || POLLRDHUP);
+	pfd.events = (POLLIN | POLLRDHUP);
 
 	pfd.revents = 0;
 
@@ -128,15 +190,11 @@ ssize_t read_all(int temporary_fd, char* buffer, char* buffer_error  , ssize_t l
 
 		ret = poll(&pfd, 1, TM_EXP_POLL);
 
-		//the following call function for now does make that much sense for the following reason:
-
-		/*we have set that the event for inturrption is POLLIN, no other events can cause the thread to be unblocked*/
-
 		pErrHand_result = handle_poll_error(&pfd, ret, POLLIN);
 
-		if(pErrHand_result == -1){
+		if(pErrHand_result != NO_SHUTDOWN){
 
-			return -1;
+			return pErrHand_result;
 
 		}
 
@@ -150,7 +208,7 @@ ssize_t read_all(int temporary_fd, char* buffer, char* buffer_error  , ssize_t l
 
 		rErrHand_result = error_handler_recv(n);
 
-		if(rErrHand_result==-1){
+		if(rErrHand_result != NO_SHUTDOWN){
 
 			return -1;
 
@@ -286,7 +344,7 @@ ssize_t receive_framed_message(int fd, char* buffer_message, char* buffer_error,
 		return -1;
 
 	}
-
+	//i don't think the following condition will ever happen, due to the experation,the socket will keep waiting untill all bytes asked are received, or when the time expeires, when time expeires a negative -1 is returned
 	if(header_bytes< (ssize_t)sizeof(net_len)){
 
 		snprintf(buffer_error, BUFFER_SIZE, "%s", error_string_holder[INIT_RECV_LEN]);
@@ -299,6 +357,8 @@ ssize_t receive_framed_message(int fd, char* buffer_message, char* buffer_error,
 
 	if((ssize_t)payload_len >= max_buf_len){
 
+		//we need to figure out what to do here still, because, the client is still going to try to send the real message, regardless if there is buffer over_flow (even thought the client is going to be programmed in order to prevent this aswell, safeguards are necessary but maybe this one is not necessary i don't know)
+
 		snprintf(buffer_error, BUFFER_SIZE,"%s",error_string_holder[MESS_RECV_LEN_OVF]);
 
 		return -1;
@@ -308,7 +368,7 @@ ssize_t receive_framed_message(int fd, char* buffer_message, char* buffer_error,
 
 	ssize_t payload_bytes = read_all(fd, buffer_message,buffer_error, (ssize_t)payload_len);
 
-	if(payload_bytes<0){
+	if(payload_bytes<=0){
 
 		return -1;
 
